72d540cc80ce0c3592dc78e22f040ea858c8826d
jnthnch <jchu127@gmail.com>
2019-04-18 11:11:30 -0700

    Create CRUD api routes

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var bodyParser  = require('body-parser');
+var bodyParser = require('body-parser');
 var cors = require('cors');
 var mongoose = require('mongoose');
 var db = require('../database-mongodb/index.js');
@@ -8,30 +8,65 @@ var Stock = require('../database-mongodb/Stock.js');
 var port = 8080;
 var app = express();
 
-
 //middleware
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
 app.use(bodyParser.json())
 
-app.use(express.static(__dirname + '/../public/'));
+app.use('/stocks/:query', express.static(__dirname + '/../public/'));
 
-app.get('/stocks/:query', (req, res) => {
+app.get('/api/stocks/:query', (req, res) => {
     var query = req.params.query;
-    var dbQuery = {ticker : req.params.query}
+    var dbQuery = {
+        ticker: req.params.query
+    }
 
-     if(parseInt(query)) {
-         dbQuery = {id : req.params.query}
-     }
-    
+    if (parseInt(query)) {
+        dbQuery = {
+            id: req.params.query
+        }
+    }
 
     Stock.find(dbQuery, (err, data) => {
-        if(err) { 
+        if (err) {
             throw err;
         }
-            res.send(JSON.stringify(data))
+        res.send(JSON.stringify(data))
     });
-    
+})
+
+//CREATE
+app.post('/stocks/create', (req, res) => {
+    let name = req.body.name;
+    let ticker = req.body.ticker;
+    let currentPrice = req.body.currentPrice;
+    Stock.create({
+        name,
+        ticker,
+        currentPrice,
+    }, () => res.send('created stock: ', req.body.name))
+})
+
+//UPDATE
+app.patch('/stocks/:tickerSymbol', (req, res) => {
+    let ticker = req.params.tickerSymbol;
+    let currentPrice = req.body.currentPrice;
+    Stock.findOneAndUpdate({
+        ticker
+    }, {
+        currentPrice
+    }, res.send('Updated stock price for: ', ticker))
+})
+
+//DELETE
+app.delete('/stocks/:tickerSymbol', (req, res) => {
+    let ticker = req.params.tickerSymbol;
+    let currentPrice = req.body.currentPrice;
+    Stock.deleteOne({
+        ticker
+    }, () => console.log('Deleted stock: ', ticker))
 })
 
 
